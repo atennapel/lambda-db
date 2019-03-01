bd1c44c604c3f2ba169fddb591d9ad5519095d2c
Albert ten Napel <aptennap@gmail.com>
2019-03-01 12:14:54 +0100

    add lambda calculus

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 const {
   vr, abs, app,
-} = require('./exprs');
+  toMinimal,
+  toNameless,
+  toNamed,
+  free,
+  reduce,
+  substFree,
+} = require('./lambda');
 
-const expr = abs(['f', 'x'], app(vr('f'), vr('x')));
+const env = {
+  I: toNameless(abs(['x'], vr('x'))),
+  K: toNameless(abs(['x', 'y'], vr('x'))),
+  S: toNameless(abs(['x', 'y', 'z'],
+    app(app(vr('x'), vr('z')), app(vr('y'), vr('z'))))),
+};
+
+const expr = app(vr('K'), vr('I'));
 console.log(`${expr}`);
+const nm = toNameless(expr);
+console.log(`${nm}`);
+console.log(`${toMinimal(nm)}`);
+const su = substFree(env, expr);
+console.log(`${su}`);
+console.log(`${toMinimal(su)}`);
+const re = reduce(su);
+console.log(`${re}`);
+console.log(`${toMinimal(re)}`);
+const nm2 = toNamed(re);
+console.log(`${nm2}`);
+
